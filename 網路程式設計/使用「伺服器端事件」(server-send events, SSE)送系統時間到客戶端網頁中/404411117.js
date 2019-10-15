27c09b445eda80b57f55a1efa34286809ba7404d
huangyuce <eric19961231@gmail.com>
2019-10-15 14:35:15 +0800

    add crt

--- 網路程式設計/使用「伺服器端事件」(server-send events, SSE)送系統時間到客戶端網頁中/404411117.js
+++ 網路程式設計/使用「伺服器端事件」(server-send events, SSE)送系統時間到客戶端網頁中/404411117.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 var url = require('url');
 var data = fs.readFileSync('page01.html', 'utf8');
 var data2 = fs.readFileSync('banner-page.html', 'utf8');
-//var privateKey = fs.readFileSync('server.key');
-//var certificate = fs.readFileSync('server.crt');
-//var server_config = {
-//    key: privateKey,
-//    cert: certificate
-//};
+var privateKey = fs.readFileSync('server.key');
+var certificate = fs.readFileSync('server.crt');
+var server_config = {
+    key: privateKey,
+    cert: certificate
+};
 var sse_header = {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
@@ -32,20 +32,22 @@ var server = web.createServer(function rout(req, res) {
     }else
         res.end('Invalid Request!');
 });
-/*var servers = webs.createServer(server_config, function rout(req, res) {
+var servers = webs.createServer(server_config, function rout(req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         res.end();
     } else if (req.url == '/student') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body>This is https</body></html>');
+        res.write('<html><body>This is http</body></html>');
         res.end();
-    } else if (req.url == '/kiwi.png') {
-        res.write(img);
+    } else if (req.url == '/sse') {
+        res.writeHead(200,sse_header);
+        res.write(`id: 1 \n`);
+        res.write(`data: Test Message ${Date.now()}\n\n`);
         res.end();
-    } else
+    }else
         res.end('Invalid Request!');
 });
-servers.listen(8000);*/
+servers.listen(8000);
 server.listen(5000);
