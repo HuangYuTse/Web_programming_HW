afcb645409a8873a38a831a55750751a4cd6b9bd
huangyuce <eric19961231@gmail.com>
2019-10-23 13:18:03 +0800

    hw3

---
+++ 網路程式設計/Homework 03 An http:https server providing a file submission form and accepting file upload/404411117.js
@@ -0,0 +1,47 @@
+const formidable = require('formidable');
+const http = require('http');
+const fs   = require('fs');
+const util = require('util'); // only used for display file info in util.inspect()
+
+http.createServer(function(req, res) {
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') { // parse a file upload
+    var form = new formidable.IncomingForm();
+    var fields = [], files = [];
+    form
+    .on('error', function(err){
+        res.writeHead(200, {'content-type':'text/plain'});
+        res.end('error:\n\n'+util.inspect(err));
+    })
+    .on('field', function(field, value){
+        console.log(field, value);
+        fields.push([field, value]);
+    })
+    .on('progress', function(bytesReceived, bytesExpected) {
+        var percent_complete = (bytesReceived / bytesExpected) * 100;
+        console.log(percent_complete.toFixed(2));
+    })
+    .on('file', function(field, file) {
+        console.log(field, file);
+        files.push([field, file]);
+    })
+    .on('end', function(){
+      console.log('-> post done');
+      res.writeHead('200', {'content-type':'text/plain'});
+      res.end('received fields:\n\n'+util.inspect(fields) +'\n\n received files: ' + util.inspect(files));
+    });
+    console.log(req);
+    form.parse(req);
+
+    return;
+  }
+  // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    'User Name: <input type="text" name="username"><br />'+
+    'Some Number: <input type="number" name="age"><br />'+
+    'Image File: <input type="file" name="photo" multiple="multiple"><br />'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+}).listen(8080);
